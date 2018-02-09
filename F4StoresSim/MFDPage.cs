4f5fa59a9922d30219de2d2b5092e35322a09b54
Blu3wolf <blue@blu3wolf.com>
2018-02-09 21:11:26 +0800

    Initial commit

---
+++ F4StoresSim/MFDPage.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Windows;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F4StoresSim
+{
+    abstract class MFDPage
+    {
+        // osblabels is a List 0-19 of OSBLabels, one for each OSB. Blank labels for blank slots.
+        List<OSBLabel> osblabels;
+
+        public List<OSBLabel> OSBLabels
+        {
+            get { return osblabels; }
+        }
+
+
+    }
+
+    class OSBLabel
+    {
+        public bool Boxed { get; set; }
+        public string Line1 { get; set; }
+        public string Line2 { get; set; }
+    }
+
+    class SelStationLabel
+    {
+
+    }
+
+    class RLabel
+    {
+
+    }
+
+    public enum MFDSymbol
+    {
+        RangeOrVelocityScale,
+        ElevationTics,
+        AzimuthMarker,
+        HorizonLine,
+        AircraftReference,
+        AzimuthSteeringBar,
+        BreakawayCue,
+        Increment,
+        Decrement,
+        TargetRangeCue,
+        Steerpoint,
+        Ownship,
+
+    }
+
+    class MFDGraphic
+    {
+        public Point CoOrds { get; set; }
+
+        public 
+    }
+}
---
+++ F4StoresSim/MFDSet.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F4StoresSim
+{
+    enum SMSModes
+    {
+        OFF, STBY, INV, MDDE, SJ, EJ, MSL, DGFT, AAM, AG, GUN, BIT
+    }
+
+    static class MFDSet
+    {
+        // MFDSet is a ViewModel object that simulates the internal state of the MFD Set. 
+        // MFDSet manipulates and reads the properties of Model objects, and has properties which are databound to by the View, either MFDBezel or MFDClean.
+
+        static int curMode;
+
+        // public properties to be something the View (MFDBezel) can databind to?
+    }
+}
